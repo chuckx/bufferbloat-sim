26ad264b8865c305e7eee666f153e841e3f2e6cd
Charles K. Lee II <chuckx@barricade.local>
2011-02-25 02:08:13 -0600

    implemented tranmission class updated setpath method to function properly

--- bufferbloat.java
+++ bufferbloat.java
@@ -1,12 +1,12 @@
 float x,y;
 float hs = 100.0;
 float ps = 20;
-float rate = 5;
-int maxPackets = 10;
+float rate = 15;
+int maxPackets = 20;
 
 Host host1;
 Host host2;
-Stream stream1;
+Transaction transaction1;
 
 // Processing.js functions
 
@@ -19,18 +19,21 @@ void setup() {
     host1 = new Host(width/6,height/2,hs);
     host2 = new Host(width/6*5,height/2,hs);
 
-    stream1 = new Stream(20);
+    transaction1 = new Transaction(maxPackets);
+    transaction1.setPath(width/6+hs/2,height/2,width/6*5-hs/2,height/2);
 }
 
 void draw() {
     background(#222222);
 
+    // draw hosts
     host1.display();
     host2.display();
 
+    // draw connection between hosts
     line(width/6,height/2,width/6*5,height/2);
 
-    stream1.display();
+    transaction1.display();
 }
 
 
@@ -59,7 +62,8 @@ class Packet {
     private float size;
     private float rate;
     private int color;
-    private int timer = -1;
+    private int timer;
+    boolean done;
 
     private float xpos;
     private float ypos;
@@ -73,12 +77,15 @@ class Packet {
         rate = _rate;
         color = _color;
         timer = _timer;
+        done = false;
     }
 
     Packet(float _size, float _rate, int _color) {
         size = _size;
         rate = _rate;
         color = _color;
+        timer = -1;
+        done = false;
     }
 
     void setPath(float _xStart, float _yStart, float _xEnd, float _yEnd) {
@@ -112,7 +119,8 @@ class Packet {
                 delta = delta + rate;
                 if (delta > end) {
                     delta = start;
-                    timer--; }
+                    timer--; 
+                }
             } else {
                 delta = delta - rate;
                 if (delta < end) {
@@ -122,28 +130,44 @@ class Packet {
             }
         }
 
+        if (timer == 0) {
+            done = true;
+        }
+
         return delta;
     }
 }
 
 class Stream {
-    Packet[] packets;
     int maxPackets;
     int packetCount;
     int timer;
+    int color;
+    boolean useTimer;
     boolean pause;
+    int packetsDone;
+    boolean[] packetState;
+    Packet[] packets;
 
-    float startx;
-    float starty;
-    float endx;
-    float endy;
+    float xStart;
+    float yStart;
+    float xEnd;
+    float yEnd;
 
     Stream (int _maxPackets) {
         maxPackets = _maxPackets;
         packetCount = 0;
         timer = 0;
-        puase = false;
+        color = #0000ff;
+        useTimer = true;
+        pause = false;
+        packetsDone = 0;
+        packetState = new boolean[maxPackets];
         packets = new Packet[maxPackets];
+    }
+
+    void setColor (int _color) {
+        color = _color;
     }
 
     void setPath(float _xStart, float _yStart, float _xEnd, float _yEnd) {
@@ -151,6 +175,14 @@ class Stream {
         yStart = _yStart;
         xEnd = _xEnd;
         yEnd = _yEnd;
+    }
+
+    void toggleTimer() {
+        if (useTimer == true) {
+            useTimer = false;
+        } else {
+            useTimer = true;
+        }
     }
 
     void togglePause() {
@@ -163,8 +195,10 @@ class Stream {
 
     void addPacket() {
         if (packetCount < maxPackets) {
-            packets[packetCount] = new Packet(ps,rate,#0000ff,1);
-            packets[packetCount].setPath(width/6+hs/2,height/2,width/6*5-hs/2,height/2);
+            // rate is a global variable
+            packets[packetCount] = new Packet(ps,rate,color,1);
+            packets[packetCount].setPath(xStart,yStart,xEnd,yEnd);
+            packetState[packetCount] = false;
             packetCount++;
         }
     }
@@ -172,13 +206,71 @@ class Stream {
     void display() {
         for (i = 0; i < packetCount; i++) {
             packets[i].display();
+            if (packetState[i] == false) {
+                if (packets[i].done == true) {
+                    packetState[i] = true;
+                    packetsDone++;                
+                }
+            }
         }
 
-        if (pause == false) {
-            timer++;
-            if (timer % 5 == 0) {
-                addPacket();
+        if (useTimer == true) {
+            if (pause == false) {
+                timer++;
+                if (timer % 5 == 0) {
+                    addPacket();
+                }
             }
+        }
+    }
+}
+
+class Transaction {
+    int maxPackets;
+    int packetsTransmitted;
+    Stream transmit;
+    Stream receive;
+
+    float xStart;
+    float yStart;
+    float xEnd;
+    float yEnd;
+
+    Transaction(int _maxPackets) {
+        maxPackets = _maxPackets;
+        packetsTransmitted = 0;
+
+        transmit = new Stream(maxPackets);
+        transmit.setColor(#0000ff);
+
+        receive = new Stream(maxPackets);
+        receive.setColor(#00ff00);
+        receive.toggleTimer();
+    }
+
+    void setPath(float _xStart, float _yStart, float _xEnd, float _yEnd) {
+        xStart = _xStart;
+        yStart = _yStart;
+        xEnd = _xEnd;
+        yEnd = _yEnd;
+
+        transmit.setPath(xStart,yStart,xEnd,yEnd);
+        receive.setPath(xEnd,yEnd,xStart,yStart);
+    }
+
+    void display() {
+        transmit.display();
+        receive.display();
+
+        if (packetsTransmitted != transmit.packetsDone) {
+            //console.log(packetsTransmitted + "," + transmit.packetsDone);
+            int packetsToAdd = transmit.packetsDone - packetsTransmitted;
+
+            for (int i=0; i < packetsToAdd; i++) {
+                receive.addPacket();
+            }
+
+            packetsTransmitted = transmit.packetsDone;
         }
     }
 }
