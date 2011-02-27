a521f83a88eaf5d6691f40727d08b947d62c4080
Charles K. Lee II <chuckx@barricade.local>
2011-02-27 03:44:23 -0600

    added ability to pause packet generation implemented web page control panel added labels to hosts

--- bufferbloat.js
+++ bufferbloat.js
@@ -1,20 +1,47 @@
 // Buffer Bloat Simulation
 
+// canvas settings
 var width = 800;
 var height = 200;
-var maxPackets = 20;
+
+// number of packets in a transmission
+var maxPackets;
 
 // packet settings
+
 var packetsize = 15;
 var packetrate = 10;
 
 $(document).ready(function () {
+    maxPackets = $("#maxpackets").val();
+
+    $("#maxpackets").change(function() {
+        maxPackets = $(this).val();
+    });
+
     var canvas = document.getElementById("bb");
     $("#bb").attr("width",width).attr("height",height);
 
     var context = canvas.getContext("2d");
 
     var simulation = new Simulation();
+    simulation.start();
+    simulation.setPause(true);
+
+    $("#start").click(function() {
+        if (simulation.done == true) {
+            simulation.restart();
+            $(this).attr("value","Pause");
+        } else {
+            if (simulation.pause == true) {
+                simulation.setPause(false);
+                $(this).attr("value","Pause");
+            } else {
+                simulation.setPause(true);
+                $(this).attr("value","Continue");
+            }
+        }
+    });
 
     (function renderLoop() {
         // clear canvas
@@ -24,6 +51,10 @@ $(document).ready(function () {
         // draw current simulation
         simulation.display(context);
 
+        if (simulation.done == true) {
+            $("#start").attr("value","Start a transmission");
+        }   
+
         // loop
         setTimeout(renderLoop,30);
     })();
@@ -31,6 +62,13 @@ $(document).ready(function () {
 
 
 function Simulation(canvas) {
+    this.done = false;
+    this.pause = true;
+}
+
+Simulation.prototype.start = function() {
+    this.done = false;
+
     // calculate positions of simulation elements
     var hostSize = 100;
     var host1x = width/6 - hostSize/2;
@@ -42,12 +80,26 @@ function Simulation(canvas) {
     this.xEnd = width/6*5 - hostSize/2;
     this.yEnd = height/2;
 
-    this.host1 = new Host(host1x,host1y,hostSize);
-    this.host2 = new Host(host2x,host2y,hostSize);
+    this.host1 = new Host("Client",host1x,host1y,hostSize);
+    this.host2 = new Host("Server",host2x,host2y,hostSize);
 
     this.transaction1 = new Transaction(maxPackets);
     this.transaction1.setPath(this.xStart,this.yStart,this.xEnd,this.yEnd);
     //transaction1.setPause(true);
+}
+
+Simulation.prototype.restart = function() {
+    delete this.host1;
+    delete this.host2;
+    delete this.transactions1;
+
+    this.start();
+}
+
+Simulation.prototype.setPause = function(state) {
+    this.pause = state;
+
+    this.transaction1.setPause(this.pause);
 }
 
 Simulation.prototype.display = function(canvas) {
@@ -62,18 +114,28 @@ Simulation.prototype.display = function(canvas) {
     canvas.closePath();
 
     this.transaction1.display(canvas);
+
+    if (this.transaction1.done == true) {
+        this.done = true;
+    }
 }
 
 
-
-function Host(x,y,size) {
+function Host(label,x,y,size) {
+    this.label = label;
     this.x = x;
     this.y = y;
     this.size = size;
+
+    this.labelx = this.x + 10;
+    this.labely = this.y + 10;
 }
 
 Host.prototype.display = function(canvas) {
     canvas.strokeRect(this.x,this.y,this.size,this.size);
+
+    canvas.textBaseline = "top";
+    canvas.strokeText(this.label,this.labelx,this.labely);
 }
 
 
@@ -201,21 +263,34 @@ function Transaction(maxPackets) {
     this.packetsTransmitted = 0;
 
     this.transmit = new Stream(maxPackets);
-    this.transmit.setColor("#00ff00");
+    this.transmit.setColor("#00ffff");
 
     this.receive = new Stream(maxPackets);
-    this.receive.setColor("#0000ff");
+    this.receive.setColor("#99ff00");
     this.receive.setTimer(false);
 }
 
 Transaction.prototype.setPath = function(xStart,yStart,xEnd,yEnd) {
+    this.done = false;
     this.xStart = xStart;
     this.yStart = yStart;
     this.xEnd = xEnd;
     this.yEnd = yEnd;
 
-    this.transmit.setPath(xStart,yStart,xEnd,yEnd);
-    this.receive.setPath(xEnd,yEnd,xStart,yStart);
+    // using global packetsize to offset the transmit and receive
+    // paths to help differentiate the traffic flows
+    this.transmit.setPath(
+        xStart,
+        yStart-packetsize/2,
+        xEnd,
+        yEnd-packetsize/2
+    );
+    this.receive.setPath(
+        xEnd,
+        yEnd+packetsize/2,
+        xStart,
+        yStart+packetsize/2
+    );
 }
 
 Transaction.prototype.setPause = function(state) {
@@ -235,5 +310,9 @@ Transaction.prototype.display = function(canvas) {
         }
 
         this.packetsTransmitted = this.transmit.packetsDone;
+    }
+
+    if (this.receive.packetsDone == maxPackets) {
+        this.done = true;
     }
 }
